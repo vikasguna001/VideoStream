9ca2ed08e9a53c8189ad9948356db8955ab9c4d6
vikasguna001 <gunavikas86@gmail.com>
2023-06-07 16:34:53 +0530

    w

--- frontend/src/component/Home.js
+++ frontend/src/component/Home.js
@@ -2,11 +2,14 @@ import React, { useEffect, useState } from "react";
 import ReactHlsPlayer from "react-hls-player";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import LiveStreamData from "./LiveStream.videourls.json";
+
 function Home() {
   const Id = useParams();
   const [VideoUrl, setVideoUrl] = useState([]);
   const GetVideo = () => {
-    axios.get(`http://localhost:5000/Video_GetData?Id=1`).then((res) => {
+    // axios.get(`http://localhost:5000/Video_GetData?Id=1`).then((res) => {
+      axios.get(`https://videostream-spol.onrender.com/Video_GetData?Id=1`).then((res) => {
       setVideoUrl(res.data.data);
     });
   };
@@ -16,6 +19,13 @@ function Home() {
   }, []);
   return (
     <>
+      {/* <ReactHlsPlayer
+        src={LiveStreamData[0].Video_Url}
+        autoPlay={false}
+        controls={true}
+        width="100%"
+        height="900px"
+      /> */}
       {VideoUrl.map((item, i) => {
         return (
           <>
--- frontend/src/component/Video.js
+++ frontend/src/component/Video.js
@@ -2,11 +2,15 @@ import React, { useEffect, useState } from "react";
 import ReactHlsPlayer from "react-hls-player";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import LiveStreamData from "./LiveStream.videourls.json";
+
 function Video() {
   const Id = useParams();
   const [VideoUrl, setVideoUrl] = useState([]);
+
   const GetVideo = () => {
-    axios.get(`http://localhost:5000/Video_GetData?Id=${Id.id}`).then((res) => {
+    axios.get(`https://videostream-spol.onrender.com/Video_GetData?Id=${Id.id}`).then((res) => {
+    // axios.get(`http://localhost:5000/Video_GetData?Id=${Id.id}`).then((res) => {
       setVideoUrl(res.data.data);
     });
   };
@@ -16,6 +20,21 @@ function Video() {
   }, []);
   return (
     <>
+      {/* {LiveStreamData.filter((fdata) => fdata.Id == Id.id).map((item, i) => {
+        return (
+          <>
+            <ReactHlsPlayer
+              src={item.Video_Url}
+              autoPlay={false}
+              controls={true}
+              width="100%"
+              height="900px"
+              key={i}
+            />
+          </>
+        );
+      })} */}
+
       {VideoUrl.map((item, i) => {
         return (
           <>
